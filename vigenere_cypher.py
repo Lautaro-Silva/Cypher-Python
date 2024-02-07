f26c33ba9ab48291f3cd84e4cbdff5f47341266f
Lautaro Silva <smiling.tarito2@gmail.com>
2024-02-07 12:14:00 -0300

    Add decryption to vigenere_cypher.py

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -1,4 +1,6 @@
-def vigenere(message, key):
+## Add Vigenere encryption method, where each character of my text is encypted using a different shift in the alphabet given by a custom_key
+
+def vigenere_v1(message, key):
     key_index = 0 # key_index keeps track of which character from the key is being used
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
@@ -24,3 +26,31 @@ custom_key = 'python'
 
 encryption = vigenere(text, custom_key)
 print(encryption)
+
+
+## Also i can implement a decryption method using the same cypher
+
+def vigenere_en_de(message, key, direction):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+    
+        # Append space to the message
+        if char == ' ':
+            encrypted_text += char
+        else:        
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+    
+    return encrypted_text
+
+## Note that here as well as the other encyption methods i can using add numerical and ASCII symbols to my alphabet
