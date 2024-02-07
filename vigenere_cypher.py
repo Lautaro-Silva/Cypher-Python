e3a608bca24793b7335221609dec8eac553f8d18
Lautaro Silva <smiling.tarito2@gmail.com>
2024-02-07 12:38:42 -0300

    Add v3 (full)

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -86,4 +86,37 @@ print(encryption)
 decryption = decrypt(encryption, custom_key)
 print(decryption)
 
-## Note that here as well as the other encyption methods i can using add numerical and ASCII symbols to my alphabet and that may solve the need for the if/else clause
+## Note that here as well as the other encyption methods i can using add numerical and ASCII symbols to my alphabet and that may solve the need for the if/else clause and the .lower()
+
+text = 'Hello World!'
+custom_key = 'python'
+
+def vigenere_v3(message, key, direction=1):
+    key_index = 0
+    ascii_characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+    final_message = ''
+
+    for char in message:       
+        # Find the right key character to encode/decode
+        key_char = key[key_index % len(key)]
+        key_index += 1
+
+        # Define the offset and the encrypted/decrypted letter
+        offset = ascii_characters.index(key_char)
+        index = ascii_characters.find(char)
+        new_index = (index + offset*direction) % len(ascii_characters)
+        final_message += ascii_characters[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+encrypted = encrypt(text, custom_key)
+print(encrypted)
+decrypted = decrypt(encrypted, custom_key)
+print(decrypted)
+
