7eb5512cd94332cf945eb6849e8906fcc3d3ac39
Lautaro Silva <smiling.tarito2@gmail.com>
2024-02-07 12:25:03 -0300

    Add v2 wirth default functions for en/decryption

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -53,4 +53,37 @@ def vigenere_en_de(message, key, direction=1):
     
     return encrypted_text
 
-## Note that here as well as the other encyption methods i can using add numerical and ASCII symbols to my alphabet
+## Solve the error of wrong encyption and decryption of special symbols and add default functions for en and decryption for clarity
+
+def vigenere_v2_with_decryption(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+    for char in message.lower():
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:        
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]  
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+    
+encryption = encrypt(text, custom_key)
+print(encryption)
+decryption = decrypt(encryption, custom_key)
+print(decryption)
+
+## Note that here as well as the other encyption methods i can using add numerical and ASCII symbols to my alphabet and that may solve the need for the if/else clause
